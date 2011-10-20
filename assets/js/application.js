9f2d1fd7c473a92285c8ab19700248abbbc6192d
Carl Hörberg <carl.hoerberg@gmail.com>
2011-10-20 18:06:44 +0200

    Snyggare

---
+++ assets/js/application.js
@@ -0,0 +1,9 @@
+//=require jquery/jquery-1.6.4.js
+//=require bootstrap/js/bootstrap-alerts.js
+//=require bootstrap/js/bootstrap-dropdown.js
+//=require bootstrap/js/bootstrap-modal.js
+//=require bootstrap/js/bootstrap-scrollspy.js
+//=require bootstrap/js/bootstrap-tabs.js
+//=require bootstrap/js/bootstrap-twipsy.js
+//=require bootstrap/js/bootstrap-popover.js
+//=require tablesorter/jquery.tablesorter.js
