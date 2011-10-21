f9bb231a30bcd1814b50dd00cd2ce6eba5b634ea
Magnus Hörberg <magnus.hoerberg@gmail.com>
2011-10-21 17:13:34 +0200

    lagt till twitter-bootstrap filerna

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,9 +1,9 @@
 //=require jquery/jquery-1.6.4.js
-//=require bootstrap/js/bootstrap-alerts.js
-//=require bootstrap/js/bootstrap-dropdown.js
-//=require bootstrap/js/bootstrap-modal.js
-//=require bootstrap/js/bootstrap-scrollspy.js
-//=require bootstrap/js/bootstrap-tabs.js
-//=require bootstrap/js/bootstrap-twipsy.js
-//=require bootstrap/js/bootstrap-popover.js
+//=require twitter-bootstrap/js/bootstrap-alerts.js
+//=require twitter-bootstrap/js/bootstrap-dropdown.js
+//=require twitter-bootstrap/js/bootstrap-modal.js
+//=require twitter-bootstrap/js/bootstrap-scrollspy.js
+//=require twitter-bootstrap/js/bootstrap-tabs.js
+//=require twitter-bootstrap/js/bootstrap-twipsy.js
+//=require twitter-bootstrap/js/bootstrap-popover.js
 //=require tablesorter/jquery.tablesorter.js
